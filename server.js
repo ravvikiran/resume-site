bf1422451b0f0c49d6b2287faf61779d93e39a41
ravvikiran <ravvikiran@gmail.com>
2021-12-25 19:47:35 +0530

    server.js changes for heroku

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+const express = require("express");
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/resume-site'));
+app.use(express.static("./dist/resume-site"));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/resume-site/index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", { root: "dist/resume-site" });
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
